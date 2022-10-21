a84f6ab49293d55aabe638da5d479b5d8428c060
Daniel Sánchez <dsanchezp998@gmail.com>
2022-10-21 01:33:34 -0700

    Extra data calculations.

--- code/reg_civil_wrangling.R
+++ code/reg_civil_wrangling.R
@@ -45,28 +45,46 @@ deaths_2011 <- as_factor(deaths_2011)
 # 2020
 hom <- function(x) ifelse(x %in% "Homicidios", 1, 0)
 deaths_2020$hom <- hom(deaths_2020$mor_viol)
-deaths_2020 <- deaths_2020 %>% group_by(sexo) %>% summarize(sum(hom)) %>% 
+deaths_2020 <- 
+  deaths_2020 %>% 
+  group_by(sexo) %>% 
+  summarize(cant = sum(hom)) %>% 
   mutate(year = 2020) 
 
 # 2019
 deaths_2019$hom <- hom(deaths_2019$mor_viol)
-deaths_2019 <- deaths_2019 %>% group_by(sexo) %>% summarize(sum(hom)) %>% 
+deaths_2019 <- 
+  deaths_2019 %>% 
+  group_by(sexo) %>% 
+  summarize(cant = sum(hom)) %>% 
   mutate(year = 2019) 
 
 # 2018
 deaths_2018$hom <- hom(deaths_2018$mor_viol)
-deaths_2018 <- deaths_2018 %>% group_by(sexo) %>% summarize(sum(hom)) %>% 
+deaths_2018 <- 
+  deaths_2018 %>% 
+  group_by(sexo) %>% 
+  summarize(cant = sum(hom)) %>% 
   mutate(year = 2018) 
 
 # 2017
 deaths_2017$hom <- hom(deaths_2017$mor_viol)
-deaths_2017 %>% group_by(sexo) %>% summarize(sum(hom)) 
+deaths_2017<- 
+  deaths_2017 %>%
+  group_by(sexo) %>% 
+  summarize(cant = sum(hom)) %>% 
+  mutate(year = 2017)
 
 # 2016
 deaths_2016$hom <- ifelse(deaths_2016$mor_viol %in% "Agreciones (Homicidios)", 1, 0)
 deaths_2016$anio_insc<-as.double(deaths_2016$anio_insc)
 deaths_2016$dia_insc<-as.double(deaths_2016$dia_insc)
-deaths_2016 %>% group_by(sexo) %>% summarize(sum(hom))
+
+deaths_2016 <-
+deaths_2016 %>% 
+  group_by(sexo) %>% 
+  summarize(cant = sum(hom)) %>% 
+  mutate(year = 2016)
 
 # 2015
 deaths_2015$hom <- ifelse(deaths_2015$mor_viol %in% "Agreciones (Homicidios)", 1, 0)
@@ -75,7 +93,11 @@ deaths_2015$fecha_nac<-as.Date(deaths_2015$fecha_nac, format = '%Y-%m-%d')
 deaths_2015$fecha_fall<-as.Date(deaths_2015$fecha_fall, format = '%Y-%m-%d')
 deaths_2015$dia_insc<-as.double(deaths_2015$dia_insc)
 
-deaths_2015 %>% group_by(sexo) %>% summarize(sum(hom))
+deaths_2015 <-
+  deaths_2015 %>% 
+  group_by(sexo) %>% 
+  summarize(cant = sum(hom)) %>% 
+  mutate(year = 2015)
 
 # 2014
 deaths_2014$hom <- ifelse(deaths_2014$mor_viol %in% "Homicidio", 1, 0)
@@ -83,7 +105,12 @@ deaths_2014$anio_insc<-as.double(deaths_2014$anio_insc)
 deaths_2014$dia_insc<-as.double(deaths_2014$dia_insc)
 deaths_2014$fecha_nac<-as.Date(deaths_2014$fecha_nac, format = '%Y-%m-%d')
 deaths_2014$fecha_fall<-as.Date(deaths_2014$fecha_fall, format = '%Y-%m-%d')
-deaths_2014 %>% group_by(sexo) %>% summarize(sum(hom))
+
+deaths_2014 <-
+  deaths_2014 %>% 
+  group_by(sexo) %>% 
+  summarize(cant = sum(hom)) %>% 
+  mutate(year = 2014)
 
 # 2013
 deaths_2013$hom <- ifelse(deaths_2013$mor_viol %in% " Homicidio", 1, 0)
@@ -91,16 +118,22 @@ deaths_2013$anio_insc<-as.double(deaths_2013$anio_insc)
 deaths_2013$dia_insc<-as.double(deaths_2013$dia_insc)
 deaths_2013$fecha_nac<-as.Date(deaths_2013$fecha_nac, format = '%Y-%m-%d')
 deaths_2013$fecha_fall<-as.Date(deaths_2013$fecha_fall, format = '%Y-%m-%d')
-deaths_2013 %>% 
+
+deaths_2013 <-
+  deaths_2013 %>% 
   group_by(sexo) %>% 
-  summarize(sum(hom))
+  summarize(cant = sum(hom)) %>% 
+  mutate(year = 2013)
 
 # 2012
 deaths_2012$hom <- hom(deaths_2012$mu_violen)
 deaths_2012$anio_insc<-as.double(deaths_2012$anio_insc)
-deaths_2012 %>% 
+
+deaths_2012 <-
+  deaths_2012 %>% 
   group_by(sexo) %>% 
-  summarize(sum(hom))
+  summarize(cant = sum(hom)) %>% 
+  mutate(year = 2012)
 
 # 2011
 deaths_2011$hom <- ifelse(deaths_2011$mu_violen %in% "Homicidio", 1, 0)
@@ -109,9 +142,12 @@ deaths_2011$anio_nac<-as.double(deaths_2011$anio_nac)
 deaths_2011$anio_fall<-as.double(deaths_2011$anio_fall)
 deaths_2011$ofi_insc<-as.double(deaths_2011$ofi_insc)
 deaths_2011$acta_insc<-as.double(deaths_2011$acta_insc)
-deaths_2011 %>% 
+
+deaths_2011 <-
+  deaths_2011 %>% 
   group_by(sexo) %>% 
-  summarize(sum(hom))
+  summarize(cant = sum(hom)) %>% 
+  mutate(year = 2011)
 
 # Total de todos los años
 
@@ -127,4 +163,16 @@ deaths_total<-
   bind_rows(deaths_2012) %>% 
   bind_rows(deaths_2011)
 
+# Agrupar por años y sexo
+
+deaths_total_yearly <-
+  deaths_total %>%  
+  group_by(year, sexo) %>%
+  summarize(cant = sum(cant))
+
+# Filtrando solo para mujeres
+
+deaths_total_yearly_women <-
+  deaths_total_yearly %>% 
+  filter(sexo == 'Mujeres')
 
